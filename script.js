5887133140b84e0514869f128759b163cf25ba40
bhaveshk-25 <bhaveshk0707@gmail.com>
2026-02-17 23:39:09 +0530

    Theme change

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function toggleTheme() {
   document.body.classList.toggle("dark-mode");
 }
+
+let themes = ["theme-blue", "theme-green", "theme-purple"];
+let currentTheme = 0;
+
+function switchTheme() {
+  // Remove existing theme classes
+  themes.forEach(theme => document.body.classList.remove(theme));
+  
+  // Apply next theme
+  currentTheme = (currentTheme + 1) % themes.length;
+  document.body.classList.add(themes[currentTheme]);
+}
